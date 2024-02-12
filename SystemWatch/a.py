bd8166a714bde290954dcc58d4c11c30224b96bc
AlexPython232 <159826773+AlexPython232@users.noreply.github.com>
2024-02-12 16:52:55 -0300

    Update a.py

--- SystemWatch/a.py
+++ SystemWatch/a.py
@@ -1,5 +1,1 @@
-factura_total = int(input('Ingrese el total de la factura: '))
 
-propina = int(input('¿Qué porcentaje de propina desea dejar? (Ingrese un número entre 0 y 100): '))
-
-print('El monto de propina es: ', propina + factura_total)
